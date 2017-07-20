58a0cfd84ec39927ef95c7716155d129d18ae089
monica18p <monica18pandey@gmail.com>
2017-07-20 11:37:53 +0530

    Initial Commit

---
+++ BluetoothPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothPOC
+//
+//  Created by Monica Pandey on 20/07/17.
+//  Copyright © 2017 Monica Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
